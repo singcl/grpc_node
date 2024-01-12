4fd55ab682743f98e59ec90158c9746b079018af
singcl <1276837689@qq.com>
2024-01-13 01:25:38 +0800

    feat: node grpc

---
+++ update_todo.js
@@ -0,0 +1,15 @@
+const client = require('./client');
+
+let todo = {
+    id: '1',
+    title: 'Hello gRPC',
+    iscompleted: true,
+};
+
+client.update(todo, (error, todo) => {
+    if (!error) {
+        console.log('Note is updated successfully', todo);
+    } else {
+        console.error(error);
+    }
+});
