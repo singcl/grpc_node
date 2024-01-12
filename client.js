c70b16a7ac09828abb2b70fd39865edd3b36a95e
singcl <1276837689@qq.com>
2024-01-13 01:40:59 +0800

    feat: update

--- client.js
+++ client.js
@@ -1,10 +1,12 @@
 const grpc = require('grpc');
 
-const PROTO_PATH = './todo.proto'
+const PROTO_PATH = './todo.proto';
 
-const TodoService = grpc.load(PROTO_PATH).TodoService
+const TodoService = grpc.load(PROTO_PATH).TodoService;
 
-const client = new TodoService('localhost:50051',
-grpc.credentials.createInsecure())
+const client = new TodoService(
+    'localhost:50051',
+    grpc.credentials.createInsecure()
+);
 
-module.exports = client
+module.exports = client;
